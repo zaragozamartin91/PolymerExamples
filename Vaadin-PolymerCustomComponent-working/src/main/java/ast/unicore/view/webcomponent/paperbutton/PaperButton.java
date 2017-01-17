2a17d7a8a6bb383b2f6056f9be99852b5da0825f
Martin <zaragozamartin91@gmail.com>
2017-01-17 11:01:02 -0300

    Se agrego "setLabel" a paperButton.
    Por error se quitaron metodos importantes del nuevo PaperButton.

--- Vaadin-PolymerCustomComponent-working/src/main/java/ast/unicore/view/webcomponent/paperbutton/PaperButton.java
+++ Vaadin-PolymerCustomComponent-working/src/main/java/ast/unicore/view/webcomponent/paperbutton/PaperButton.java
@@ -1,6 +1,7 @@
 package ast.unicore.view.webcomponent.paperbutton;
 
 import com.vaadin.annotations.JavaScript;
+import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.ui.AbstractJavaScriptComponent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -9,7 +10,7 @@ import com.vaadin.ui.JavaScriptFunction;
 import elemental.json.JsonArray;
 
 /**
- * Componente de vaadin del lado del servidor representante del componente paper-button.
+ * Componente de vaadin del lado del servidor representante del componente paper-button de polymer.
  * 
  * @author martin.zaragoza
  *
@@ -18,8 +19,6 @@ import elemental.json.JsonArray;
 @JavaScript({ "paper-button-connector.js" })
 public final class PaperButton extends AbstractJavaScriptComponent {
 	private Button wrappedButton = new Button();
-	private String backColor = "white";
-	private String currStyle = "btn waves-effect waves-teal";
 
 	/**
 	 * Manejador de eventos Click.
@@ -53,6 +52,16 @@ public final class PaperButton extends AbstractJavaScriptComponent {
 	}
 
 	/**
+	 * Establece el label del boton.
+	 * 
+	 * @param label
+	 *            Texto del boton.
+	 */
+	public void setLabel(String label) {
+		getState().buttonLabel = label;
+	};
+
+	/**
 	 * Agrega un clickListener.
 	 * 
 	 * @param listener
@@ -69,6 +78,21 @@ public final class PaperButton extends AbstractJavaScriptComponent {
 
 		return this;
 	}
+
+	// FIXME : NO FUNCIONA.
+	// /**
+	// * Agrega un atajo de presion de boton.
+	// *
+	// * @param keyCode
+	// * Tecla a presionar.
+	// * @param modifiers
+	// * [the (optional) modifiers for invoking the shortcut, null for
+	// * none].
+	// *
+	// */
+	// public void setClickShortcut(int keyCode, int... modifiers) {
+	// wrappedButton.setClickShortcut(keyCode, modifiers);
+	// }
 
 	@Override
 	protected PaperButtonState getState() {
@@ -98,52 +122,6 @@ public final class PaperButton extends AbstractJavaScriptComponent {
 		setEnabled(true);
 	}
 
-	/**
-	 * Marca al boton como de tipo floating.
-	 * 
-	 */
-	public void setAsFloating() {
-		currStyle = "btn-floating btn-large waves-effect waves-light";
-		getState().buttonStyle = buildStyle();
-	}
-
-	/**
-	 * Marca el boton como regular.
-	 * 
-	 */
-	public void setAsRegular() {
-		currStyle = "btn waves-effect waves-light ";
-		getState().buttonStyle = buildStyle();
-	}
-
-	/**
-	 * Marca al boton como plano.
-	 */
-	public void setAsFlat() {
-		currStyle = "waves-effect waves-teal btn-flat";
-		getState().buttonStyle = currStyle;
-	}
-
-	/**
-	 * Obtiene el string de color de fondo del boton.
-	 * 
-	 * @return Color de fondo del boton.
-	 */
-	public String getBackColor() {
-		return backColor;
-	}
-
-	/**
-	 * Establece el color de fondo del boton.
-	 * 
-	 * @param backColor
-	 *            Color a establecer.
-	 */
-	public void setBackColor(String backColor) {
-		this.backColor = backColor;
-		getState().buttonStyle = buildStyle();
-	}
-
 	@Override
 	public void setHeight(float height, Unit unit) {
 		throw new UnsupportedOperationException("Operacion no soportada!");
@@ -169,9 +147,5 @@ public final class PaperButton extends AbstractJavaScriptComponent {
 				wrappedButton.click();
 			}
 		});
-	}
-
-	private String buildStyle() {
-		return currStyle + " " + backColor;
 	}
 }
